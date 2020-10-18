1288a3aca3c7653ea5178b3af5e4ff3d5cde1574
Abdullah Althobetey <abdullah.th@me.com>
2020-10-18 10:02:19 +0300

    Initial Commit

---
+++ MathQuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathQuizGame
+//
+//  Created by Abdullah Althobetey on 18/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
