5525657b0ac0b42822ec37b05f89f626bcc45462
Abdullah Althobetey <abdullah.th@me.com>
2020-10-18 10:41:19 +0300

    render question’s options

--- MathQuizGame/QuestionViewController.swift
+++ MathQuizGame/QuestionViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 class QuestionViewController: UIViewController {
     
     @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     let question: String
+    let options: [String]
     
-    init(question: String) {
+    init(question: String, options: [String]) {
         self.question = question
+        self.options = options
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -24,7 +27,22 @@ class QuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self 
         headerLabel.text = question
     }
 
+}
+
+extension QuestionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+    
+    
 }
