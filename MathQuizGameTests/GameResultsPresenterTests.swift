d2d7285efc7a29a804055c64692e8dd8ad48c9a2
Abdullah Althobetey <abdullah.th@me.com>
2020-11-04 16:33:16 +0300

    DRY GameResultPresenterTests

--- MathQuizGameTests/GameResultsPresenterTests.swift
+++ MathQuizGameTests/GameResultsPresenterTests.swift
@@ -12,42 +12,45 @@ import GameEngine
 class GameResultsPresenterTests: XCTestCase {
 
     func test_summary_withTwoQuestionsAndScoreOne_returnCorrectSummary() {
-        let answers = [
-            Question(value: "Q1"): "A1",
-            Question(value: "Q2"): "wrong"
-        ]
-        
-        let gameResult = GameResult(answers: answers, score: 1)
-        let sut = GameResultPresenter(gameResult: gameResult, correctAnswers: [:])
+        let sut = makeSUT(
+            userAnswers: [
+                Question(value: "Q1"): "A1",
+                Question(value: "Q2"): "wrong"
+            ],
+            score: 1
+        )
         
         XCTAssertEqual(sut.summary, "You got 1 of 2 correct answers")
     }
     
     func test_summary_withThreeQuestionsAndScoreTwo_returnCorrectSummary() {
-        let answers = [
-            Question(value: "Q1"): "A1",
-            Question(value: "Q2"): "A2",
-            Question(value: "Q3"): "wrong"
-        ]
-        let gameResult = GameResult(answers: answers, score: 2)
-        let sut = GameResultPresenter(gameResult: gameResult, correctAnswers: [:])
+        let sut = makeSUT(
+            userAnswers: [
+                Question(value: "Q1"): "A1",
+                Question(value: "Q2"): "A2",
+                Question(value: "Q3"): "wrong"
+            ],
+            score: 2
+        )
         
         XCTAssertEqual(sut.summary, "You got 2 of 3 correct answers")
     }
     
     func test_presentableAnswers_withGameResultEmpty_presentableAnswersShouldBeEmpty() {
-        let answers = [Question<String>: String]()
-        let gameResult = GameResult(answers: answers, score: 0)
-        let sut = GameResultPresenter(gameResult: gameResult, correctAnswers: [:])
+        let sut = makeSUT(
+            userAnswers: [:],
+            score: 0
+        )
         
         XCTAssertTrue(sut.presentableAnswers.isEmpty)
     }
     
     func test_presentableAnswers_withGameResultOneCorrectAnswer_presentableAnswersMappedCorrectly() {
-        let answers = [Question(value: "Q1"): "A1"]
-        let correctAnswers = [Question(value: "Q1"): "A1"]
-        let gameResult = GameResult(answers: answers, score: 1)
-        let sut = GameResultPresenter(gameResult: gameResult, correctAnswers: correctAnswers)
+        let sut = makeSUT(
+            userAnswers: [Question(value: "Q1"): "A1"],
+            correctAnswers: [Question(value: "Q1"): "A1"],
+            score: 1
+        )
         
         XCTAssertEqual(sut.presentableAnswers.count, 1)
         XCTAssertEqual(sut.presentableAnswers.first?.question, "Q1")
@@ -56,15 +59,29 @@ class GameResultsPresenterTests: XCTestCase {
     }
     
     func test_presentableAnswers_withGameResultOneWrongAnswer_presentableAnswersMappedCorrectly() {
-        let answers = [Question(value: "Q3"): "A9"]
-        let correctAnswers = [Question(value: "Q3"): "A3"]
-        let gameResult = GameResult(answers: answers, score: 1)
-        let sut = GameResultPresenter(gameResult: gameResult, correctAnswers: correctAnswers)
+        let sut = makeSUT(
+            userAnswers: [Question(value: "Q3"): "A9"],
+            correctAnswers: [Question(value: "Q3"): "A3"],
+            score: 0
+        )
         
         XCTAssertEqual(sut.presentableAnswers.count, 1)
         XCTAssertEqual(sut.presentableAnswers.first?.question, "Q3")
         XCTAssertEqual(sut.presentableAnswers.first?.answer, "A3")
         XCTAssertEqual(sut.presentableAnswers.first?.wrongAnswer, "A9")
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        userAnswers: [Question<String>: String],
+        correctAnswers: [Question<String>: String] = [:],
+        score: Int
+    ) -> GameResultPresenter {
+        
+        let gameResult = GameResult(answers: userAnswers, score: score)
+        return GameResultPresenter(gameResult: gameResult, correctAnswers: correctAnswers)
     }
 
 }
