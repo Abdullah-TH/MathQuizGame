22021804eef5046ebdfa730e92ca92051ff33d90
Abdullah Althobetey <abdullah.th@me.com>
2020-11-18 22:04:31 +0300

    Extract setup code into helpers in iOSViewControllerFactoryTests

--- MathQuizGameTests/iOSViewControllerFactoryTests.swift
+++ MathQuizGameTests/iOSViewControllerFactoryTests.swift
@@ -16,25 +16,47 @@ class iOSViewControllerFactoryTests: XCTestCase {
     let answerOne = "A1"
     let answerTwo = "A2"
     
+    var questions: [Question<String>] {
+        [questionOne, questionTwo]
+    }
+    
+    var options: [Question<String>: [String]] {
+        [
+            questionOne: [answerOne, answerTwo],
+            questionTwo: [answerOne, answerTwo]
+        ]
+    }
+    
+    var correctAnswers: [Question<String>: String] {
+        [
+            questionOne: answerOne,
+            questionTwo: answerTwo
+        ]
+    }
+    
     func test_questionViewController_createsQuestionViewControllerWithTitle() {
-        let questionPresenter = QuestionPresenter(
-            questions: [questionOne],
-            question: questionOne
+        let sut = makeSUT(
+            questions: questions,
+            options: options
         )
-        let (_, questionVC) = makeSUT(
-            questions: [questionOne],
+        
+        let (questionVC, questionPresenter) = makeQuestionViewControllerAndPresenter(
             question: questionOne,
-            options: [answerOne, answerTwo]
+            factory: sut
         )
         
         XCTAssertEqual(questionVC.title, questionPresenter.title)
     }
 
     func test_questionViewController_createsQuestionViewControllerWithCorrectQuestionAndOptions() {
-        let (_, questionVC) = makeSUT(
-            questions: [questionOne],
+        let sut = makeSUT(
+            questions: questions,
+            options: options
+        )
+        
+        let (questionVC, _) = makeQuestionViewControllerAndPresenter(
             question: questionOne,
-            options: [answerOne, answerTwo]
+            factory: sut
         )
         
         XCTAssertEqual(questionVC.question, questionOne.value)
@@ -42,25 +64,16 @@ class iOSViewControllerFactoryTests: XCTestCase {
     }
     
     func test_gameResultViewController_createResultsViewControllerWithSummary() {
-        let questions = [questionOne, questionTwo]
-        let factory = iOSViewControllerFactory(
+        let sut = makeSUT(
             questions: questions,
-            options: [
-                questionOne: [answerOne, answerTwo],
-                questionTwo: [answerOne, answerTwo]
-            ],
-            correctAnswers: [
-                questionOne: answerOne,
-                questionTwo: answerTwo
-            ]
+            options: options,
+            correctAnswers: correctAnswers
         )
-        let userAnswers = [questionOne: answerOne, questionTwo: answerOne] // 1 of 2 is correct
-        let gameResults = GameResult(answers: userAnswers, score: 1)
-        let resultVC = factory.gameResultViewController(for: gameResults) as! GameResultsViewController
-        let resultPresenter = GameResultPresenter(
-            questions: questions,
-            gameResult: gameResults,
-            correctAnswers: [questionOne: answerOne, questionTwo: answerTwo]
+        
+        let (resultVC, resultPresenter) = makeResultViewControllerAndPresenter(
+            userAnswers: [questionOne: answerOne, questionTwo: answerOne], // 1 of 2 is correct,
+            score: 1,
+            factory: sut
         )
         
         XCTAssertEqual(resultVC.summary, resultPresenter.summary)
@@ -71,18 +84,50 @@ class iOSViewControllerFactoryTests: XCTestCase {
     
     private func makeSUT(
         questions: [Question<String>],
-        question: Question<String>,
-        options: [String],
+        options: [Question<String> : [String]],
         correctAnswers: [Question<String> : String] = [:]
-    ) -> (iOSViewControllerFactory, QuestionViewController) {
+    ) -> iOSViewControllerFactory {
         
         let sut = iOSViewControllerFactory(
             questions: questions,
-            options: [questionOne: options],
+            options: options,
             correctAnswers: correctAnswers
         )
-        let questionVC = sut.questionViewController(for: question, answerCallback: { _ in }) as! QuestionViewController
-        return (sut, questionVC)
+        return sut
+    }
+    
+    private func makeQuestionViewControllerAndPresenter(
+        question: Question<String>,
+        factory: iOSViewControllerFactory
+    ) -> (QuestionViewController, QuestionPresenter) {
+        
+        let presenter = QuestionPresenter(
+            questions: questions,
+            question: questionOne
+        )
+        
+        let questionVC = factory.questionViewController(
+            for: question,
+            answerCallback: { _ in }
+        ) as! QuestionViewController
+        
+        return (questionVC, presenter)
+    }
+    
+    private func makeResultViewControllerAndPresenter(
+        userAnswers: [Question<String>: String],
+        score: Int,
+        factory: iOSViewControllerFactory
+    ) -> (GameResultsViewController, GameResultPresenter) {
+        
+        let gameResults = GameResult(answers: userAnswers, score: score)
+        let presenter = GameResultPresenter(
+            questions: questions,
+            gameResult: gameResults,
+            correctAnswers: correctAnswers
+        )
+        let resultVC = factory.gameResultViewController(for: gameResults) as! GameResultsViewController
+        return (resultVC, presenter)
     }
     
 }
