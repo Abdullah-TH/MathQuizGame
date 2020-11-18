bcbe725742ca1cf2aebe87a0db288fe7b760f825
Abdullah Althobetey <abdullah.th@me.com>
2020-11-18 22:25:17 +0300

    Move Question from the Presenter to the GameEngine

---
+++ GameEngine/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  GameEngine
+//
+//  Created by Abdullah Althobetey on 18/11/2020.
+//
+
+import Foundation
+
+public struct Question<T: Hashable>: Hashable {
+    
+    public let value: T
+    
+    public init(value: T) {
+        self.value = value
+    }
+}
--- MathQuizGameTests/QuestionPresenterTests.swift
+++ MathQuizGameTests/QuestionPresenterTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import MathQuizGame
+import GameEngine
 
 class QuestionPresenterTests: XCTestCase {
     
