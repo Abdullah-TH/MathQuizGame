259174c07e80ab4a611c3118464b0bb7a5a1674a
Abdullah Althobetey <abdullah.th@me.com>
2020-11-22 22:05:40 +0300

    Create a QuizDelegate in QuizEngine and deprecate Router

--- QuizEngine/Sources/QuizEngine/Router.swift
+++ QuizEngine/Sources/QuizEngine/Router.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public protocol QuizDelegate {
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func route(to question: Question, answerCallback: @escaping (Answer) -> Void)
+    func route(to result: GameResult<Question, Answer>)
+}
+
+@available(*, deprecated)
 public protocol Router {
     associatedtype Question: Hashable
     associatedtype Answer
